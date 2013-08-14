8a5d220d917be3587ccd7272a8857cafc76cc805
warmflasha <warmflasha@gmail.com>
2013-08-14 01:38:39 -0400

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ ruby '1.9.3'
 gem 'rails', '4.0.0'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'bcrypt-ruby', '3.0.1'
+gem 'faker', '1.1.2'
+gem 'will_paginate'
+gem 'bootstrap-will_paginate'
 
 group :development, :test do
   gem 'sqlite3', '1.3.7'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+
+  before_action :signed_in_user, only: [:edit, :update, :index]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
   
   def show
   	@user = User.find(params[:id])
@@ -6,6 +10,16 @@ class UsersController < ApplicationController
 
   def new
   	@user=User.new
+  end
+
+  def index
+    @users=User.paginate(page: params[:page])
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed"
+    redirect_to users_url
   end
 
   def create
@@ -19,10 +33,42 @@ class UsersController < ApplicationController
   	end
   end
 
+  def edit
+    #Not needed because it is included in the before action
+    #@user = User.find(params[:id])
+  end
+
+  def update
+    #@user=User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private 
 
   	def user_params
   		params.require(:user).permit(:name,:email,:password,:password_confirmation)
   	end
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in." 
+      end
+    end
+
+    def correct_user
+        @user= User.find(params[:id])
+        redirect_to(root_url) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin? 
+    end
 
 end
